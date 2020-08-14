2baef41fa254a0b0f6323a3cb443fc1b9b97474d
vijay_venu <vijayvenu1997@gmail.com>
2020-08-14 13:41:12 +0530

    postgres config hero added

--- blog_flask/config.py
+++ blog_flask/config.py
@@ -1,13 +1,12 @@
 import os
-email = os.environ.get("EMAIL")
-password = os.environ.get("PASSWORD")
-postgres_user = os.environ.get("POSTGRES_USER")
-postgres_password = os.environ.get("POSTGRES_PASSWORD")
+email = "message.mail2051@gmail.com"
+password = "suji2051"
+postgres = os.environ.get['DATABASE_URL']
 
 class Config:
     SECRET_KEY = '4caf8d2be717983770c235607a2fa9f7dab4b4b3fc13f7679c54e3d698ee5c45'
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{postgres_user}:{postgres_password}@localhost/flask_blog"
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = postgres
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG=True
     MAIL_SERVER='smtp.gmail.com'
     MAIL_PORT=587
