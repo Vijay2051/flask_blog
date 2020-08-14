3240a708c78885886f3c1836c8a1260e4e843b0d
vijay_venu <vijayvenu1997@gmail.com>
2020-08-14 13:43:50 +0530

    postgres config hero added1

--- blog_flask/config.py
+++ blog_flask/config.py
@@ -1,7 +1,7 @@
 import os
 email = "message.mail2051@gmail.com"
 password = "suji2051"
-postgres = os.environ.get['DATABASE_URL']
+postgres = os.environ.get('DATABASE_URL')
 
 class Config:
     SECRET_KEY = '4caf8d2be717983770c235607a2fa9f7dab4b4b3fc13f7679c54e3d698ee5c45'
